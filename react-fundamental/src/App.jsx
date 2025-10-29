147570a79e9a3e791c5449f530634ace069b17c2
alfimonth <alvy1610@gmail.com>
2025-10-29 15:23:32 +0700

    feat: router

--- react-fundamental/src/App.jsx
+++ react-fundamental/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import HomePage from "./pages/index.jsx";
 import { GlobalContext } from "./context/index.js";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routers/index.jsx";
 
 const App = () => {
   const user = {
@@ -9,7 +11,7 @@ const App = () => {
   return (
     <>
       <GlobalContext.Provider value={user}>
-        <HomePage />
+        <RouterProvider router={router} />
       </GlobalContext.Provider>
     </>
   );
