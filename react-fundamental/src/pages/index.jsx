185bb37b657f3bc37634e98b47c5e21904bbd05c
alfimonth <alvy1610@gmail.com>
2025-10-29 14:48:45 +0700

    fetch api

--- react-fundamental/src/pages/index.jsx
+++ react-fundamental/src/pages/index.jsx
@@ -1,10 +1,18 @@
 import postsData from "../posts.json";
 import Article from "../components/Article";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../components/Search";
 
 const HomePage = () => {
   const [posts, setPosts] = useState(postsData);
+  const [externalPosts, setExternalPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setExternalPosts(json));
+  }, []);
+
   const filterHandler = (keyword) => {
     const filteredPosts = postsData.filter((item) =>
       item.title.toLowerCase().includes(keyword.toLowerCase())
@@ -15,8 +23,12 @@ const HomePage = () => {
     <>
       <h1>Simple Blog</h1>
       <Search filterHandler={filterHandler} totalPosts={posts.length} />
-      {posts.map((blog, index) => (
+      {/* {posts.map((blog, index) => (
         <Article key={index} {...blog} />
+      ))} */}
+      <hr />
+      {externalPosts.map((item, index) => (
+        <div key={index}>- {item.title}</div>
       ))}
     </>
   );
