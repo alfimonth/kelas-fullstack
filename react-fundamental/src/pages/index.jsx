035a750f2a129798bd39aa942cc3616cde97e8b4
alfimonth <alvy1610@gmail.com>
2025-10-29 11:19:12 +0700

    feat(home): filter handler

--- react-fundamental/src/pages/index.jsx
+++ react-fundamental/src/pages/index.jsx
@@ -1,15 +1,22 @@
 import posts from "../posts.json";
 import Article from "../components/Article";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [keyword, setKeyword] = useState();
+
+  const filterHandler = (e) => {
+    setKeyword(e.target.value);
+  };
   return (
     <>
       <h1>Simple Blog</h1>
       <div>
-        Cari Article: <input></input>
+        Find Article: <input onChange={filterHandler} />
       </div>
-      {posts.map((blog) => (
-        <Article {...blog} />
+      {keyword && <small>0 result for keyword {keyword}</small>}
+      {posts.map((blog, index) => (
+        <Article key={index} {...blog} />
       ))}
     </>
   );
