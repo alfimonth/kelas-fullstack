5b1742293dd852bdcc9fe140b540f0bb0fbe33ee
alfimonth <alvy1610@gmail.com>
2025-10-29 15:46:20 +0700

    feat: root layout

--- react-fundamental/src/routers/index.jsx
+++ react-fundamental/src/routers/index.jsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages";
 import About from "../pages/about";
+import RootLayout from "../layouts/RootLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/about",
-    element: <About />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
