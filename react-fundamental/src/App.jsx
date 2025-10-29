1532516c0393bbac27ca5750d993f3b1de4e192a
alfimonth <alvy1610@gmail.com>
2025-10-29 10:52:55 +0700

    call home page from app

--- react-fundamental/src/App.jsx
+++ react-fundamental/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Article from "./components/Article.jsx";
+import HomePage from "./pages/index.jsx";
 
 const App = () => {
   return (
     <>
-      <Article name="Alfi" titles={["NextJs", "ReactJS", "NodeJS"]} />
-      <hr />
-      <Article name="Hasan" titles={["NuxtJs", "VueJS", "Nginx"]} />
+      <HomePage />
     </>
   );
 };
