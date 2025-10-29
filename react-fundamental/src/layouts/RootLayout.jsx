d0eeaf9ae1a5a6edec85f6ae67fcc44822b1a2ce
alfimonth <alvy1610@gmail.com>
2025-10-29 16:08:05 +0700

    feat: blog

--- react-fundamental/src/layouts/RootLayout.jsx
+++ react-fundamental/src/layouts/RootLayout.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 const RootLayout = () => {
   return (
     <>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      <Link to="/">Home</Link> | <Link to="/blog">Blog</Link> | <Link to="/about">About</Link>
       <p />
       <Outlet />
     </>
--- react-fundamental/src/routers/index.jsx
+++ react-fundamental/src/routers/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages";
 import About from "../pages/about";
 import RootLayout from "../layouts/RootLayout";
+import Blog from "../pages/blogs";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
       },
       {
         path: "/about",
